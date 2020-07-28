39c68edc0aadaf47a24acaadbc5078e39b7c3e99
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-28 12:14:12 +0530

    chnage1

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -10,7 +10,7 @@ namespace TempleteAddingExample.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View11();
         }
 
         public IActionResult AddBook()
